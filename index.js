72faac584e8867ac3a8cd1646f53ea390ac3dfb9
said shah <saidmysr20@aol.com>
2019-02-04 22:25:02 -0500

    added instructions

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+//
+// Misa Amane loves Light Yagami and she wants to join him in his mission to wipe out all criminals. However, Light Yagami is skeptical about her intelligence and asks her to prove it.
+//
+// He gives Misa a string consisting of lowercase english characters and asks her to count the occurrence of the non-empty prefix which occurs maximum number of times in the string as substring. Misa cannot solve it and has asked for your help. Solve this task for her!
+//
+// Input Format
+//
+// The first and only line of input contains the string as defined in statement.
+//
+// Constraints
+//
+// It is guaranteed that the given string contains only lowercase english alphabets
+// Output Format
+//
+// In a single line print the count of the prefix which occurs maximum number of times.
+//
+// Sample Input 0
+//
+// abc
+// Sample Output 0
+//
+// 1
+// Sample Input 1
+//
+// abbcdabbcd
+// Sample Output 1
+//
+// 2
+
+
+
+
+
 function misasLoveSubmission(input){
   let str=""
     let splitString = input.split("")
